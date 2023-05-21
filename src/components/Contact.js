a702a748b28150c761c758ab6bb55cfcbba85bc2
Chris Schreiner <chrisjschreiner@gmail.com>
2023-05-21 07:52:34 -0700

    removed form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,54 +1,27 @@
 import React from "react";
-import "../styles/Contact.css";
-import me3 from "../assets/me3.jpg";
-import { Fade } from "react-reveal";
+import "../styles/Contact.scss";
 
 function Contact() {
   return (
-    <div className="contact component__space" id="contact">
-      <div className="row">
-        <Fade bottom>
-          <div className="col__2">
-            {/* <div className="contact__box"> */}
-            {/* <div className="contact__meta"> */}
-            <h1 className="hire__text underline">Contact</h1>
-            <p className="hire__text white">
-              I am available for freelance work. Connect with me via phone:
-            </p>
-            <p className="hire__text white">
-              <strong>541-633-9456</strong> or email{" "}
-              <strong>chrisjschreiner@gmail.com</strong>
-            </p>
-            {/* </div> */}
-            {/* <div className="input__box"> */}
-            <input type="text" className="contact name" placeholder="Name" />
-            <input type="text" className="contact email" placeholder="Email" />
-            <input
-              type="text"
-              className="contact subject"
-              placeholder="Subject"
-            />
-            <textarea
-              name="message"
-              id="message"
-              placeholder="Message"
-            ></textarea>
-            <button className="btn contact pointer" type="submit">
-              Submit
-            </button>
-            {/* </div> */}
-            {/* </div> */}
-          </div>
-        </Fade>
-        <Fade bottom>
-          <div className="col__2">
-            {/* <div>
-              <img src={me3} alt="" className="contact__img" />
-            </div> */}
-          </div>
-        </Fade>
+    <section id="contact" className="contact__container">
+      <h2 className="contact__heading">What's Next?</h2>
+      <h2 className="title">Get In Touch</h2>
+      <p className="contact__blurb">
+        Thanks for stopping by! I'm currently looking to join a creative and
+        passionate team of developers. If you think we'd be a good fit, email me
+        at chrisjschreiner@gmail.com, or click below to say hello!
+      </p>
+      <div>
+        <a
+          className="contact__link"
+          href="mailto:chrisjschreiner@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Say Hello
+        </a>
       </div>
-    </div>
+    </section>
   );
 }
 
